d0692c32df1cd4fc4b1961ad86227a6db32ee90e
Adam Weissman <adam@comedytragedyepic.com>
2019-09-21 10:12:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,13 @@
-def Person
-  attr_accessor :name
-  
+class Person
   def initialize(name)
     @name = name
   end
-  
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def breed
+    @name
+  end
 end
